b90cd07155b4d3bf5d8bf8bb952a70dfb10189a6
kumagai-school <yoshii@kumagai-c.com>
2025-06-30 16:20:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -218,6 +218,18 @@ if recent_high and recent_low:
             st.warning("高値＞安値 の数値を正しく入力してください。")
 
 st.markdown("""
+<div style='color:red; font-size:14px;'>
+<ul>
+  <li>※ピックアップチャートの銘柄については、あくまで「ルール1」銘柄のレッスンとなります。</li>
+  <li>※特定の取引を推奨するものではなく、銘柄の助言ではございません。</li>
+  <li>※本サービスは利益を保証するものではなく、投資にはリスクが伴います。投資の際は自己責任でよろしくお願いいたします。</li>
+</ul>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("---")
+
+st.markdown("""
 <div style='
     text-align: center;
     color: gray;
